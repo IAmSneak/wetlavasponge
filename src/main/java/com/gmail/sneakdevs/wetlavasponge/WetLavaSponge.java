0ce0e4545a8e0cf71cbb9956d76a88a8a88338d3
IAmSneak <66091019+IAmSneak@users.noreply.github.com>
2021-09-14 20:09:36 -0400

    Initial commit

---
+++ src/main/java/com/gmail/sneakdevs/wetlavasponge/WetLavaSponge.java
@@ -0,0 +1,11 @@
+package com.gmail.sneakdevs.wetlavasponge;
+
+import net.fabricmc.api.ModInitializer;
+
+public class WetLavaSponge implements  ModInitializer {
+    public static final String Mod_ID = "wetlavasponge";
+
+    @Override
+    public void onInitialize() {
+    }
+}
